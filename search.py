77dcffbfec69963b13ba06132b08e96d655384f1
NastyaMor42 <88672490+NastyaMor42@users.noreply.github.com>
2026-04-30 17:06:36 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,1 +1,26 @@
+import requests
+import streamlit as st
 
+def search_hikes(query):
+    url = "https://serpapi.com/search.json"
+
+    params = {
+        "q": query,
+        "hl": "he",
+        "gl": "il",
+        "api_key": st.secrets["SERPAPI_KEY"]
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    results = []
+
+    for r in data.get("organic_results", [])[:5]:
+        results.append({
+            "title": r.get("title"),
+            "snippet": r.get("snippet"),
+            "link": r.get("link")
+        })
+
+    return results
